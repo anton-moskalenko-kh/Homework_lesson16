6bb65c3bc08e0ff747ca7b573f016f76b1d182e1
anton-moskalenko-kh <anton.moskalenko.kh@gmail.com>
2022-06-23 21:08:22 +0300

    Homework_ver.1.1.0

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
     $('#next').on('click', function () {
-        const currentItem = $('.active');
-        const nextItem = currentItem.next();
+        const $currentItem = $('.active');
+        const $nextItem = $currentItem.next();
 
-        if (nextItem.length) {
-            currentItem.removeClass('active');
-            nextItem.addClass('active')
+        if ($nextItem.length) {
+            $currentItem.removeClass('active');
+            $nextItem.addClass('active')
         }
     })
 
     $('#prev').on('click', function () {
-        const currentItem = $('.active');
-        const prevItem = currentItem.prev();
+        const $currentItem = $('.active');
+        const $prevItem = $currentItem.prev();
 
-        if (prevItem.length) {
-            currentItem.removeClass('active');
-            prevItem.addClass('active')
+        if ($prevItem.length) {
+            $currentItem.removeClass('active');
+            $prevItem.addClass('active')
         }
     })
 })
